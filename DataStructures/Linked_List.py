8792bfef953ea9f5a33c3d89da5a2e33d037fb9e
xtbtds <liza.xtbtds@gmail.com>
2022-04-11 16:45:14 +0300

    added insert in linked list class

--- DataStructures/Linked_List.py
+++ DataStructures/Linked_List.py
@@ -23,6 +23,33 @@ class LinkedList:
             print(a.data)
             a = a.next
 
+    #_______________________INSERT_________________________
+    def push(self, new_data):     #insert in the beginning of the linked list (O(1) time)
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, new_data):     #insert after given node (O(1) time)
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def append(self, new_data):      #insert at the end (O(n) time)
+        new_node = Node(new_data)
+        if self.head == None:        #if linked list is empty
+            self.head = new_node
+            return
+
+        last = self.head            #traverse through the linked list until we find node with next == none
+        while (last.next):
+            last = last.next
+        last.next = new_node
+
+    #_______________________DELETE_________________________
+
+
+
+
 if __name__=='__main__':
     list1 = LinkedList()
 
@@ -37,5 +64,13 @@ if __name__=='__main__':
     #link from second to third
     second.next = third
 
+    list1.push(4)
+    list1.insert_after_node(second, 5)
+    list1.append(6)
+    list1.insert_after_node(list1.head, 57)
+
     #print linked list
     list1.print_llist()
+
+
+
