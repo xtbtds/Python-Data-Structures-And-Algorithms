e01a8eb6e9e5fc81a7f9ed0b8ae441fee239b3c3
xtbtds <51954806+xtbtds@users.noreply.github.com>
2023-02-25 13:42:12 +0100

    Create Merge_sorted_array.py

---
+++ Arrays/Merge_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i1=m-1
+        i2=n-1
+        index=m+n-1
+        while i2 >= 0:
+            if i1 >= 0 and nums1[i1] > nums2[i2]:
+                nums1[index] = nums1[i1]
+                i1 -= 1
+                index -= 1
+            else:
+                nums1[index] = nums2[i2]
+                i2 -= 1
+                index -= 1
+            
